55e954d635a920b4b49745a89ed07be7485438e3
Your Name <lpondboy@gmail.com>
2020-12-25 20:57:54 +0900

    fix:ドアの半分より上のブロックを壊すとドアが破壊されるのを修正

--- src/main/java/life/midorin/info/lifers/listeners/BlockBreakListener.java
+++ src/main/java/life/midorin/info/lifers/listeners/BlockBreakListener.java
@@ -2,26 +2,53 @@ package life.midorin.info.lifers.listeners;
 
 import life.midorin.info.lifers.manager.ProtectManager;
 import life.midorin.info.lifers.protect.Protect;
+import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.material.Door;
 
 public class BlockBreakListener implements Listener {
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent e) {
 
-        final Player player = e.getPlayer();
-        final Block block = e.getBlock();
+        Block block = e.getBlock();
+
+        //保護対象のブロックか？
+        if(!ProtectManager.get().PROTECTABLE_MATERIALS.contains(block.getType())) return;
+
+        switch (block.getType()) {
+
+            case WOOD_DOOR:
+            case SPRUCE_DOOR:
+            case BIRCH_DOOR:
+            case JUNGLE_DOOR:
+            case ACACIA_DOOR:
+            case DARK_OAK_DOOR:
+            case TRAP_DOOR:
+            case WOODEN_DOOR:
+            case IRON_DOOR:
+            case IRON_TRAPDOOR:
+
+                BlockState blockState = block.getState();
+                Door door = (Door) blockState.getData();
+
+                if (door.isTopHalf()) {
+                    block = Bukkit.getWorld(block.getWorld().getUID()).getBlockAt(block.getLocation().subtract(0, 1, 0));
+                }
+
+                break;
+        }
 
         //保護されているブロックか？
         if(!ProtectManager.get().isProtect(block.getLocation())) return;
 
         final Protect protect = ProtectManager.get().getProtected_Block(block.getLocation());
-
-        if(protect == null) return;
+        final Player player = e.getPlayer();
 
         //オーナーではないならキャンセル
         if(!protect.isOwner(player.getName())) {
