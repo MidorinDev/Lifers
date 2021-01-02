74c9e1f53843803c9d3e12d9eeef7fd1d1114732
Your Name <lpondboy@gmail.com>
2021-01-02 16:36:48 +0900

    chore:新しい初期設定

--- src/main/resources/schema/sqlite.sql
+++ src/main/resources/schema/sqlite.sql
@@ -15,3 +15,14 @@ CREATE TABLE IF NOT EXISTS `Protected_block_Members` (
     `protected_blockId` VARCHAR(16)         NOT NULL,
     `uuid`              VARCHAR(36)         NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS `Homes` (
+    `id`    INTEGER PRIMARY KEY NOT NULL,
+    `uuid`  VARCHAR(36)         NOT NULL,
+    `world` VARCHAR(64)         NOT NULL,
+    `x`     BIGINT              NOT NULL,
+    `y`     BIGINT              NOT NULL,
+    `z`     BIGINT              NOT NULL,
+    `Yaw`   BIGINT              NOT NULL,
+    `Pitch` BIGINT              NOT NULL,
+);
